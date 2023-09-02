1e96b386d6c4843ca346573570a29e9d0167cdb9
firecaneB <doorumi123@gmail.com>
2023-09-02 15:16:55 +0900

    2

---
+++ dhk/2/main.js
@@ -0,0 +1,29 @@
+let operatorBtn = document.querySelectorAll('.operator')
+
+for(let x of operatorBtn){
+  x.addEventListener('click',function(){
+    operator.innerText = x.innerText
+  })
+}
+
+calculator.addEventListener('click', function(){  
+  if( !/^[0-9]+$/.test(num1.value+num2.value) ){
+    result.innerText = '숫자가 제대로 입력되지 않았습니다.'  
+    return
+  }
+  switch(operator.innerText){
+    case '+':
+      result.innerText = 1 * num1.value + 1 * num2.value
+      break
+    case '-':
+      result.innerText = num1.value - num2.value
+      break
+    case '*':
+      result.innerText = num1.value * num2.value
+      break
+    case '/':
+      result.innerText = num1.value / num2.value
+      break
+  }
+})
+
