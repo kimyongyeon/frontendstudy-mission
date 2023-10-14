1534348dc72ad725fb9f5756e155a06ce31586e4
es-kyy <es-kyy@smilegate.com>
2023-10-14 19:04:02 +0900

    숙제 해주세요.

--- mje/mission/eventDelegation/util.js
+++ mje/mission/eventDelegation/util.js
@@ -1,3 +1,65 @@
+const util = {
+  ceateAndSetElement: function (tagName, className, innerHTML) {
+    const element = document.createElement(tagName);
+    if (className) {
+      element.className = className;
+    }
+    if (innerHTML) {
+      element.innerHTML = innerHTML;
+    }
+    return element;
+  },
+};
+util.ceateAndSetElement("div", "box", "hello");
+
+const util2 = (function () {
+  function createAndSetElement(tagName, className, innerHTML) {
+    const element = document.createElement(tagName);
+    if (className) {
+      element.className = className;
+    }
+    if (innerHTML) {
+      element.innerHTML = innerHTML;
+    }
+    return element;
+  }
+  return {
+    createAndSetElement: createAndSetElement,
+  };
+})();
+util2.createAndSetElement("div", "box", "hello");
+
+function test() {}
+
+function Test() {}
+
+Test.prototype.createAndSetElement = function (
+  tagName,
+  className,
+  innerHTML
+) {};
+
+class ParentUtil {
+  constructor() {}
+  createAndSetElement(tagName, className, innerHTML) {
+    const element = document.createElement(tagName);
+    if (className) {
+      element.className = className;
+    }
+    if (innerHTML) {
+      element.innerHTML = innerHTML;
+    }
+    return element;
+  }
+}
+
+class ChildUtil extends ParentUtil {}
+
+const util3 = new Util(); // file.js
+const util4 = new Util(); // file2.js
+util3.createAndSetElement("div", "box", "hello");
+
+export default util;
 // DOM 요소 생성 및 속성 설정 함수
 export function createAndSetElement(tagName, className, innerHTML) {
   const element = document.createElement(tagName);
